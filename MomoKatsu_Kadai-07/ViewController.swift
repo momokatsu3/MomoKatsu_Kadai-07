427b927f2108af1de0e500b761672fb919e2f6ae
momokatsu3 <moka.oct10@gmail.com>
2023-05-08 13:05:01 +0900

    Pull Version

--- MomoKatsu_Kadai-07/ViewController.swift
+++ MomoKatsu_Kadai-07/ViewController.swift
@@ -4,12 +4,6 @@
 //
 //  Created by モモカツ on 2023/05/06.
 //  Part7 足し算と引き算を行うタブアプリ
-// 【Swift/Xcode】UITabBarControllerの実装方法・コンプリートガイド
-// https://ios-docs.dev/uitabbar/
-// Text Fieldで数値と小数点のみのキーボードを表示
-// https://swiftintroduction.hatenablog.com/entry/2020/04/20/211059
-// 【Swift】TextFieldのキーボードを閉じる方法3選
-// https://qiita.com/saka2jp/items/4b8b5f2297910d7f3d1b
 
 import UIKit
 
@@ -32,8 +26,6 @@ class AdditionViewController: UIViewController {
         // テキストフィールドへ文字入力した場合、"０"とする。
         let value1 = Int(textField1.text ?? "") ?? 0
         let value2 = Int(textField2.text ?? "") ?? 0
-//        let calcResult = value1 + value2
-//        calcResultLabel.text = "計算結果：" + String( calcResult )
         calcResultLabel.text = "計算結果：" + String( value1 + value2 )
      }
 }
@@ -51,8 +43,6 @@ class SubtractionViewController: UIViewController {
         // テキストフィールドへ文字入力した場合、"０"とする。
         let value1 = Int(textField1.text ?? "") ?? 0
         let value2 = Int(textField2.text ?? "") ?? 0
-//        let calcResult = value1 - value2
-//        calcResultLabel.text = "計算結果：" + String( calcResult )
         calcResultLabel.text = "計算結果：" + String( value1 - value2 )
      }
 }
